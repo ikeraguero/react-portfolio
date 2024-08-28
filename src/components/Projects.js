9ec8a11f7ff1fae0b0141607d39aa6c8353a2176
ikeraguero <ikerpires407@gmail.com>
2024-08-27 21:56:08 -0300

    Add Reusable Projects Component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,20 @@ import project2 from "../img/project2.png";
 
 const projects = [
   {
-    name: "Childcare Brasil",
+    name: "Childcare Brasil 🧒",
+    descPostion: "right",
     description:
       "Plataforma desenvolvida para uma organização beneficente fictícia. Nela, é possível realizar doações em dinheiro para crianças e escolas carentes de todo o Brasil. O website oferece uma interface que, além de realizar doações, possibilita cadastrar, remover e editar crianças e escolas através da conta administradora.",
-    stack: ["vue", "node", "express", "mongoDB"],
+    stack: ["Vue", "Node", "Express", "MongoDB"],
+    image: project1,
   },
   {
-    name: "Fitmap",
+    name: "Fitmap 🗺️",
+    descPostion: "left",
     description:
       "Plataforma que possibilita o usuário acompanhar e registrar sua rotina de exercícios feitos em uma determinada região, possibilitando que adicione marcadores ao mapa de sua localidade atual renderizada utilizando a API Geolocation e a biblioteca Leaflet.",
-    stack: ["javascript vanilla", "poo"],
+    stack: ["JavaScript Vanilla", "POO"],
+    image: project2,
   },
 ];
 
@@ -25,25 +29,36 @@ export default function Projects() {
       <h3 className="projects-text">PORTFOLIO</h3>
       <h4>Projetos em que já trabalhei 🧩</h4>
       <div className="project-list">
-        <div className="project">
+        {projects.map((project) => (
+          <Project
+            name={project.name}
+            description={project.description}
+            stack={project.stack}
+            image={project.image}
+            direction={project.descPostion}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function Project({ name, description, stack, image, direction }) {
+  return (
+    <div className="project">
+      {direction === "right" ? (
+        <>
           <div className="project-image">
-            <img src={project1} alt="" className="img" />
+            <img src={image} alt="" className="img" />
           </div>
           <div className="project-text">
-            <span className="project-title">Childcare Brasil 🧒</span>
-            <p className="project-description">
-              Plataforma desenvolvida para uma organização beneficente fictícia.
-              Nela, é possível realizar doações em dinheiro para crianças e
-              escolas carentes de todo o Brasil. O website oferece uma interface
-              que, além de realizar doações, possibilita cadastrar, remover e
-              editar crianças e escolas através da conta administradora.
-            </p>
+            <span className="project-title">{name}</span>
+            <p className="project-description">{description}</p>
             <p className="project-stack">
               <ul className="stack-list">
-                <li>Vue</li>
-                <li>Node</li>
-                <li>Express</li>
-                <li>MongoDB</li>
+                {stack.map((item) => (
+                  <li>{item}</li>
+                ))}
               </ul>
             </p>
             <div className="project-links">
@@ -63,43 +78,41 @@ export default function Projects() {
               </a>
             </div>
           </div>
-        </div>
-        <div className="project">
+        </>
+      ) : (
+        <>
           <div className="project-text">
-            <span className="project-title">Fitmap 🗺️</span>
-            <p className="project-description">
-              Plataforma que possibilita o usuário acompanhar e registrar sua
-              rotina de exercícios feitos em uma determinada região,
-              possibilitando que adicione marcadores ao mapa de sua localidade
-              atual renderizada utilizando a API Geolocation e a biblioteca
-              Leaflet.
-            </p>{" "}
-            {/* Properly closed <p> tag */}
+            <span className="project-title">{name}</span>
+            <p className="project-description">{description}</p>
             <p className="project-stack">
               <ul className="stack-list">
-                <li>JavaScript Vanilla</li>
-                <li>POO</li>
+                {stack.map((item) => (
+                  <li>{item}</li>
+                ))}
               </ul>
             </p>
             <div className="project-links">
-              <a href="https://github.com/ikeraguero/fitmap" target="_blank">
+              <a
+                href="https://github.com/ikeraguero/childcare-brasil"
+                target="_blank"
+              >
                 <ion-icon
                   name="logo-github"
                   className="logo-project"
                 ></ion-icon>
                 <span>Github</span>
               </a>
-              <a href="https://ikeraguero-fitmap.netlify.app/" target="_blank">
+              <a href="https://childcarebrasil.netlify.app/" target="_blank">
                 <ion-icon name="create-outline"></ion-icon>
                 <span>Deploy</span>
               </a>
             </div>
           </div>
           <div className="project-image">
-            <img src={project2} alt="" className="img" />
+            <img src={image} alt="" className="img" />
           </div>
-        </div>
-      </div>
-    </section>
+        </>
+      )}
+    </div>
   );
 }
